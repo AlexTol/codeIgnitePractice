ce56a9633df78fa670521180a4ee366fdae8c230
Alex <atol514@gmail.com>
2019-02-15 22:15:51 -0800

    my message

--- mTut/application/controllers/Pages.php
+++ mTut/application/controllers/Pages.php
@@ -10,7 +10,19 @@
 
         public function index()
         {
-            echo 'Hello World';
+            //echo 'Hello World';
+            $data['title'] = 'Home Page';
+            $data['message'] = 'Hello World From Alex!';
+            //load in our views in the order we want
+                //old way
+            //$this->load->view('Pages/Elements/page_header', $data); 
+            //$this->load->view('Pages/Elements/navigation'); 
+            //$this->load->view('Pages/home', $data);
+            //$this->load->view('Pages/Elements/footer'); 
+
+            //using master layout
+            $this->load->view('layout/master', $data);
+            
         }
     }
 ?>
---
+++ mTut/application/views/Pages/Elements/page_header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" type="text/css" 
+        href="<?php echo base_url();?>assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" 
+        href="<?php echo base_url();?>assets/css/spacing.css" />
+    <link rel="stylesheet" type="text/css" 
+        href="<?php echo base_url();?>assets/css/custom.css" />
+    <title><?php echo $title; ?></title>
+</head>
+
+<body>
